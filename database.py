0fbee35dcd4558a5ebb4cb4839c03f3ce5111d3f
Isaac S <8111@balcarras.gloucs.sch.uk>
2025-06-03 11:21:18 +0000

    databases added

---
+++ database.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+db_name = "minibus_booking.db"
+
+#connect to sqlite database and create the database if it doesn't exist
+
+conn = sqlite3.connect(db_name)
+
+#control structure via a cursor
+
+cursor = conn.cursor()
+
+#create the database and it's fields - field name data type ad rules of the column / field
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        phone TEXT UNIQUE
+        
+                ); 
+
+""")
+
+#create mini_buses table
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS mini_buses(
+        bus_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        plate_number TEXT UNIQUE NOT NULL,
+        capacity INTEGER NOT NULL
+                );
+
+""")
+
+#create bookings table
+
+cursor.execute(""" 
+    CREATE TABLE IF NOT EXISTS bookings (
+        booking_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        bus_id INTEGER NOT NULL,
+        booking_date TEXT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+        FOREIGN KEY (bus_id) REFERENCES mini_buses (bus_id)
+               
+               );
+
+""")
