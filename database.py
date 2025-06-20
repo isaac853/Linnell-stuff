b89c43f8c9208563c6bd8d8e8f6123bcb49a0615
Isaac S <8111@balcarras.gloucs.sch.uk>
2025-06-20 10:00:20 +0000

    put stuff in database

--- database.py
+++ database.py
@@ -59,8 +59,29 @@ cursor = conn.cursor()
 
 #creates a tuple inside a list
 users = [
-    ("Alice Smith", "alice@example.com","079621231234")
-    ("Bob Jones","bob@example.com","078457582344")
+    ("Alice Smith", "alice@example.com","079621231234"),
+    ("Bob Jones","bob@example.com","078457582344"),
     ("Charlie Lee", "charlie@bobmail.com","075959595404")
 
 ]
+
+buses = [
+    ("VU27QYT", 12),
+    ("HU21ABC", 15),
+    ("KV23OPW", 10)
+]
+
+bookings = [
+    (1, 1, "25-03-25"),
+    (2, 2, "27-24-25"),
+    (3, 1, "23-01-25")
+]
+
+#insert the data from the lists into the data structure
+
+cursor.executemany("INSERT INTO users (name, email, phone) VALUES (?,?,?)", users)
+cursor.executemany("INSERT INTO mini_buses (plate_number, capacity) VALUES (?,?)", buses)
+cursor.executemany("INSERT INTO bookings (user_id, bus_id, booking_date) VALUES (?,?,?)", bookings)
+
+conn.commit()
+conn.close()
